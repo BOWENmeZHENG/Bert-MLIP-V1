4e1d0c4f3286da2b14e1ebeaf220810a5b73453b
BOWENmeZHENG <bowen_zheng@berkeley.edu>
2024-03-06 14:57:50 -0800

    forward batch data

--- utils_data.py
+++ utils_data.py
@@ -13,11 +13,14 @@ def list2token(tokenizer, text_list, max_length):
     token = torch.tensor(token_list_padded)[None, :]
     return token
 
-def read_data(json_file):
+def read_data(json_file, max_length):
     data_list = []
     with open(json_file) as f:
         for jsonObj in f:
             record = json.loads(jsonObj)
+            if len(record['words']) > max_length:
+                record['words'] = record['words'][:max_length]
+                record['ner'] = record['ner'][:max_length]
             data_list.append(record)
     return data_list
 
